901c0aacb62dc96c4e3677687397b453098fedf9
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-04-14 22:10:14 +0900

    Update app.module.ts
    configModule / typeormModule 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
+import { BinanceModule } from './binance/binance.module';
+import { RedisModule } from './redis/redis.module';
+import { UpbitModule } from './upbit/upbit.module';
+import { WebsocketModule } from './websocket/websocket.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+            validationSchema: Joi.object({
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+                DB_CHARSET: Joi.string().required(),
+                DB_TIMEZONE: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRoot({
+            name: process.env.DB_NAME,
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            charset: process.env.DB_CHARSET,
+            timezone: process.env.DB_TIMEZONE,
+            synchronize: true,
+            logging: true,
+            entities: [__dirname + '*/entity/*{.ts}'],
+        }),
+        UpbitModule,
+        RedisModule,
+        WebsocketModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
