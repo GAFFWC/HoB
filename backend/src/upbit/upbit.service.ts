a63a54aa36db869dbfb36af2c97544e26dfc2863
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-04-14 22:11:46 +0900

    Create upbit.service.ts

---
+++ backend/src/upbit/upbit.service.ts
@@ -0,0 +1,13 @@
+import { HttpService, Inject } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { Tokenize } from './decorator/tokenize.decorator';
+
+export class UpbitService {
+    constructor(@Inject(HttpService) private readonly http: HttpService) {}
+
+    private baseUrl = 'https://api.upbit.com/v1/';
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, { timeZone: 'Asia/Seoul' })
+    async init() {}
+
+    async getCoins(@Tokenize() payload: any) {}
+}
