7c350f8ce44917cedce496c1d8354c3851339d39
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-04-20 22:54:38 +0900

    Update websocket.module.ts

--- backend/src/websocket/websocket.module.ts
+++ backend/src/websocket/websocket.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RedisModule } from 'src/redis/redis.module';
+import { RedisService } from 'src/redis/redis.service';
+import { UpbitModule } from 'src/upbit/upbit.module';
 import { WebSocketIOGateway } from './websocket.gateway';
 import { WebsocketService } from './websocket.service';
 
 @Module({
+    imports: [RedisModule, UpbitModule],
     providers: [WebSocketIOGateway, WebsocketService],
 })
 export class WebsocketModule {}
