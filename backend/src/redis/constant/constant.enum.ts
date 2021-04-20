1fde849bff2444e2c5dfd9d9598a591ce1723351
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-04-20 22:53:01 +0900

    Update constant.enum.ts

--- backend/src/redis/constant/constant.enum.ts
+++ backend/src/redis/constant/constant.enum.ts
@@ -1,6 +1,4 @@
 export enum RedisExpression {
-    SYMBOLS_KRW = 'SYMBOLS_KRW',
-    SYMBOLS_BTC = 'SYMBOLS_BTC',
-    PRICE_KRW = 'PRICE_KRW',
-    PRICE_BTC = 'PRICE_BTC',
+    SYMBOLS = 'SYMBOLS',
+    PRICE = 'PRICE',
 }
